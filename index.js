2d337946dbe1e0527197eca48af314e13df52e6f
Vladimir de Turckheim <vlad2t@hotmail.com>
2018-06-05 23:47:25 +0200

    write comments

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
 'use strict';
-const CLient = require('./lib/client');
-
-const client = new CLient(require('./secret').hackerone_user, require('./secret').hackerone_token);
-
-const run = async function () {
-
-    const id = 361880;
-    const res = await client.get_report(id);
-    console.log(res);
-
-    const res2 = await client.write_comment(id);
-
-};
-
-run()
-.catch(console.log)
+const CLient = module.exports = require('./lib/client');
